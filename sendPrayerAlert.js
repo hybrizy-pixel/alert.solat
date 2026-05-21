b0b935dbf7c1948e0c50561b2055de187deb2daa
Safwan <hybrizy@gmail.com>
2026-05-21 16:14:05 +0800

    tahik

--- sendPrayerAlert.js
+++ sendPrayerAlert.js
@@ -5,157 +5,34 @@ res
 
 try{
 
+// =========================
+// CRON START
+// =========================
+
 console.log(
 "🕌 CRON RUNNING..."
 );
 
 
 // =========================
-// GET MESSAGE
+// DEBUG TEST
 // =========================
-
-const message =
-req.query.message ||
-"🕌 MY SOLAT";
-
-
-// =========================
-// GET SUBSCRIPTION ID
-// =========================
-
-const subscriptionId =
-req.query.subscriptionId;
-
-if(!subscriptionId){
-
-return res.status(400).json({
-
-success:false,
-
-error:
-"No subscriptionId"
-
-});
-
-}
-
 
 console.log(
-"SUBSCRIPTION:",
-subscriptionId
+"✅ CRON WEBSITE HIT API"
 );
 
 
 // =========================
-// SEND PUSH
-// =========================
-
-const response =
-await fetch(
-
-"https://api.onesignal.com/notifications",
-
-{
-
-method:"POST",
-
-headers:{
-
-"Content-Type":
-"application/json",
-
-Authorization:
-`Basic ${process.env.ONESIGNAL_API_KEY}`
-
-},
-
-body:JSON.stringify({
-
-app_id:
-process.env.ONESIGNAL_APP_ID,
-
-
-// =========================
-// TARGET USER DEVICE
-// =========================
-
-include_subscription_ids:[
-subscriptionId
-],
-
-
-// =========================
-// NOTIFICATION CONTENT
-// =========================
-
-headings:{
-en:"🕌 MY SOLAT"
-},
-
-contents:{
-en:message
-},
-
-
-// =========================
-// PUSH SETTINGS
-// =========================
-
-chrome_web_icon:
-"https://solatmys.vercel.app/icon-192.png",
-
-chrome_web_badge:
-"https://solatmys.vercel.app/icon-192.png",
-
-small_icon:
-"https://solatmys.vercel.app/icon-192.png",
-
-large_icon:
-"https://solatmys.vercel.app/icon-512.png",
-
-ios_badgeType:
-"Increase",
-
-ios_badgeCount:
-1,
-
-
-// =========================
-// OPEN APP WHEN CLICK
-// =========================
-
-url:
-"https://solatmys.vercel.app"
-
-})
-
-}
-
-);
-
-
-// =========================
-// RESPONSE DATA
-// =========================
-
-const data =
-await response.json();
-
-console.log(
-"ONESIGNAL RESPONSE:",
-data
-);
-
-
-// =========================
-// SUCCESS
+// RETURN TEST
 // =========================
 
 return res.status(200).json({
 
 success:true,
 
-onesignal:data
+message:
+"CRON WORKING"
 
 });
 
